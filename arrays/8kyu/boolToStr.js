73deff733b01d04336d68265a15a252de7bfec1d
Karlygash Yakiyayeva <53092602+karlakz@users.noreply.github.com>
2022-08-03 23:28:59 +0600

    [ref]: Restructure folders

--- arrays/8kyu/boolToStr.js
+++
@@ -1,5 +0,0 @@
-// Link to the task:
-// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
-const boolToWord = (bool) => (bool ? "Yes" : "No");
-
-module.exports = boolToWord;
--- arrays/8kyu/tests/boolToStr.test.js
+++
@@ -1,9 +0,0 @@
-const boolToWord = require("../boolToStr");
-
-test("Convert Boolean To String", () => {
-  expect(boolToWord(true)).toBe("Yes");
-});
-
-test("Convert Boolean To String", () => {
-  expect(boolToWord(false)).toBe("No");
-});
